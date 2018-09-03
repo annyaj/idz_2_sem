3d0b6b88c1d5245632de0f2ff227736f449fe950
annecha562 <annecha562@gmail.com>
2018-09-03 17:59:12 +0600

    Added functions in Pr2

--- IDZ/Source2.cpp
+++ IDZ/Source2.cpp
@@ -38,21 +38,168 @@ void insertion_sort(int cols, int rows, Matrix& a)
 	for (int i = 0; i < rows; i++)cout << sum_array[i] << " ";
 }
 
-int main()
+void Rand_filling(Matrix& a)
 {
 	srand(time(0));
-	Matrix a(3, 3);
-	for (int i = 0; i < 3; i++)
+	int cols = a.get_cols();
+	int rows = a.get_rows();
+	for (int i = 0; i < cols; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < rows; j++)
 		{
-			int elem = rand() % 10;
+			double elem = rand() % 100;
 			a.set_elem(i, j, elem);
 			cout << elem << " ";
 		}
 		cout << endl;
 	}
-	insertion_sort(3, 3, a);
+}
+
+void Keyboard_filling(Matrix& a)
+{
+	int cols = a.get_cols();
+	int rows = a.get_rows();
+	double elem;
+	for (int i = 0; i < cols; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			cout << "Введите элемент."<<endl;
+			cin >> elem;
+			a.set_elem(i,j,elem);
+		}
+	}
+	for (int i = 0; i < cols; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			a.get_elem(i,j,elem);
+			cout << elem <<" ";
+		}
+		cout << endl;
+	}
+}
+
+void Filling_from_file(fstream& f, Matrix& a)
+{
+	int cols = a.get_cols();
+	int rows = a.get_rows();
+	double elem;
+	if (!f.is_open())return;
+	else
+	{
+		for (int i = 0; i < cols; i++)
+		{
+			for (int j = 0; j < rows; j++)
+			{
+				f >> elem;
+				a.set_elem(i, j, elem);
+			}
+		}
+		for (int i = 0; i < cols; i++)
+		{
+			for (int j = 0; j < rows; j++)
+			{
+				a.get_elem(i, j, elem);
+				cout << elem << " ";
+			}
+			cout << endl;
+		}
+	}
+}
+
+void Save_to_file(fstream& f, Matrix& a)
+{
+	int cols = a.get_cols();
+	int rows = a.get_rows();
+	double elem;
+	if (!f.is_open())return;
+	else
+	{
+		f << "Матрица" << endl << endl;
+		for (int i = 0; i < cols; i++)
+		{
+			for (int j = 0; j < rows; j++)
+			{
+				a.get_elem(i, j, elem);
+				f << elem << " ";
+			}
+			f << endl;
+		}
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL,"Rus");
+	char name[30];
+	int cols, rows;
+	cout << "Введите размеры матрицы." << endl;
+	cin >> cols >> rows;
+	Matrix a(cols, rows);
+	cols = a.get_cols();
+	rows = a.get_rows();
+	int choice=1;
+	while (choice != 7)
+	{
+		system("cls");
+
+		cout << "1. Заполнение матрицы случайными числами." << endl;
+		cout << "2. Заполнение матрицы с клавиатуры." << endl;
+		cout << "3. Заполнение матрицы с файла." << endl;
+		cout << "4. Редактор." << endl;
+		cout << "5. Сортировка вставками." << endl;
+		cout << "6. Сохранение матрицы в файл. " << endl;
+		cout << "7. Выход." << endl;
+		cout << "Введите пункт:" << endl << endl;
+		cin >> choice;
+
+		if (choice == 1)
+		{
+			Rand_filling(a);
+			system("pause");
+		}
+		if (choice == 2)
+		{
+			Keyboard_filling(a);
+			system("payse");
+
+		}
+		if (choice == 3)
+		{
+			system("payse");
+			cout << endl << "Введите имя файла" << endl;
+			cin >> name;
+			fstream file(name, ios::in);
+			Filling_from_file(file,a);
+
+		}
+		if (choice == 4)
+		{
+
+			//Element(lines, cols, A);
+		}
+		if (choice == 5)
+		{
+
+			insertion_sort(cols,rows,a);
+			system("pause");
+		}
+		if (choice == 6)
+		{
+			cout << "Введите имя файла";
+			cin >> name;
+			fstream file(name, ios::out);
+			Save_to_file(file,a);
+			file.close();
+		}
+		if (choice == 7)
+		{
+			break;
+		}
+	}
+
 	_getch();
 	return 0;
 }
